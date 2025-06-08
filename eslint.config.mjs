244cad7983d3490456936b33d6de99bf4926d997
thekaroe-thailand <thekaroe@gmail.com>
2025-06-08 17:30:50 +0700

    test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-
+  {
+    ignores: [
+      'app/generated/**',
+      'node_modules/**',
+      '.next/**',
+    ],
+  },
 ];
 
 export default eslintConfig;
